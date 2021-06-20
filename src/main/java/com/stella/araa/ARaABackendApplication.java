150ae643ab19bbd1fbe09719c2abd438a8c7ba62
Stella D <stella@stella-pc.stella>
2021-06-20 15:54:51 +0300

    Initial version

---
+++ src/main/java/com/stella/araa/ARaABackendApplication.java
@@ -0,0 +1,13 @@
+package com.stella.araa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ARaABackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ARaABackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stella/araa/ARaABackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stella.araa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ARaABackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
